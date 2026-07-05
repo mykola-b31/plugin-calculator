0eccd99ccba8fc0c7a1b5fceb48c3b85d2bc1845
Mykola Babko <babkokolya81@gmail.com>
2026-07-05 20:44:15 +0300

    ui: fix status bar overlap and card background artifacts

--- app/src/main/java/com/github/mykolab31/plugincalculator/ui/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/github/mykolab31/plugincalculator/ui/calculator/CalculatorScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -74,6 +75,7 @@ fun CalculatorScreenContent(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .systemBarsPadding()
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
